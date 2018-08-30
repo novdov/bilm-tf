8693713f0c28aff9f48ef648465013b4613c6f4f
novdov <rpwjd56@gmail.com>
2018-08-30 10:51:23 +0900

    pep8 correct

--- bin/train_elmo.py
+++ bin/train_elmo.py
@@ -26,43 +26,46 @@ def main(args):
     n_train_tokens = 605918
 
     options = {
-     'bidirectional': True,
-     'char_cnn': {'activation': 'relu',
-      'embedding': {'dim': 16},
-      'filters': [[1, 32],
-       [2, 32],
-       [3, 64],
-       [4, 128],
-       [5, 256],
-       [6, 512],
-       [7, 1024]],
-      'max_characters_per_token': 50,
-      'n_characters': 261,
-      'n_highway': 2},
+        'bidirectional': True,
+        'char_cnn': {
+            'activation': 'relu',
+            'embedding': {'dim': 16},
+            'filters': [
+                [1, 32],
+                [2, 32],
+                [3, 64],
+                [4, 128],
+                [5, 256],
+                [6, 512],
+                [7, 1024]
+            ],
+            'max_characters_per_token': 50,
+            'n_characters': 261,
+            'n_highway': 2
+        },
 
-     'dropout': 0.1,
-    
-     'lstm': {
-      'cell_clip': 3,
-      'dim': 4096,
-      'n_layers': 2,
-      'proj_clip': 3,
-      'projection_dim': 512,
-      'use_skip_connections': True},
-    
-     'all_clip_norm_val': 10.0,
-    
-     'n_epochs': 10,
-     'n_train_tokens': n_train_tokens,
-     'batch_size': batch_size,
-     'n_tokens_vocab': vocab.size,
-     'unroll_steps': 20,
-     'n_negative_samples_batch': 8192,
+        'dropout': 0.1,
+        'lstm': {
+            'cell_clip': 3,
+            'dim': 4096,
+            'n_layers': 2,
+            'proj_clip': 3,
+            'projection_dim': 512,
+            'use_skip_connections': True
+        },
+
+        'all_clip_norm_val': 10.0,
+        'n_epochs': 10,
+        'n_train_tokens': n_train_tokens,
+        'batch_size': batch_size,
+        'n_tokens_vocab': vocab.size,
+        'unroll_steps': 20,
+        'n_negative_samples_batch': 8192,
     }
 
     prefix = args.train_prefix
-    data = BidirectionalLMDataset(prefix, vocab, test=False,
-                                      shuffle_on_load=True)
+    data = BidirectionalLMDataset(prefix, vocab,
+                                  test=False, shuffle_on_load=True)
 
     tf_save_dir = args.save_dir
     tf_log_dir = args.save_dir
@@ -77,4 +80,3 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     main(args)
-
